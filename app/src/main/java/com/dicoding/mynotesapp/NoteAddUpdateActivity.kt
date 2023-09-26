79b5e48d04d47a4da7617748143c85dd841152bb
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-26 16:45:48 +0700

    Edit NoteAddUpdateActivity.kt

--- app/src/main/java/com/dicoding/mynotesapp/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/dicoding/mynotesapp/NoteAddUpdateActivity.kt
@@ -1,11 +1,35 @@
 package com.dicoding.mynotesapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.mynotesapp.databinding.ActivityNoteAddUpdateBinding
+import com.dicoding.mynotesapp.db.NoteHelper
+import com.dicoding.mynotesapp.entity.Note
 
-class NoteAddUpdateActivity : AppCompatActivity() {
+class NoteAddUpdateActivity : AppCompatActivity(), View.OnClickListener {
+    private var isEdit = false
+    private var note: Note? = null
+    private var position: Int = 0
+    private lateinit var noteHelper: NoteHelper
+
+    private lateinit var binding: ActivityNoteAddUpdateBinding
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val EXTRA_POSITION = "extra_position"
+        const val RESULT_ADD = 101
+        const val RESULT_UPDATE = 201
+        const val RESULT_DELETE = 301
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_add_update)
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
