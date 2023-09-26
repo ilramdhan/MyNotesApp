b69567c6d18a6aeda5666637dac8e584a16a6462
Ilham Ramadhan <ilhamram332@gmail.com>
2023-09-26 16:23:02 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/mynotesapp/NoteAddUpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.mynotesapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
